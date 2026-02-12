f1638eea673327316dfe676e9aaf2e763ebd8bbc
jpirnay <jens@pirnay.com>
2026-02-12 17:45:12 +0100

    Introduce deprecated warnings

--- lib/Logging/Logging.h
+++ lib/Logging/Logging.h
@@ -2,34 +2,46 @@
 
 #include <HardwareSerial.h>
 
-// Define ENABLE_SERIAL_LOG to enable logging
-// Can be set in platformio.ini build_flags or as a compile definition
+/*
+Define ENABLE_SERIAL_LOG to enable logging
+Can be set in platformio.ini build_flags or as a compile definition
 
-// Define LOG_LEVEL to control log verbosity:
-// 0 = ERR only
-// 1 = ERR + INF
-// 2 = ERR + INF + DBG
-// If not defined, defaults to 0
+Define LOG_LEVEL to control log verbosity:
+0 = ERR only
+1 = ERR + INF
+2 = ERR + INF + DBG
+If not defined, defaults to 0
+
+If you have a legitimate need for raw Serial access (e.g., binary data,
+special formatting), use the underlying logSerial object directly:
+    logSerial.printf("Special case: %d\n", value);
+    logSerial.write(binaryData, length);
+
+The logSerial reference (defined below) points to the real Serial object and
+won't trigger deprecation warnings.
+*/
 
 #ifndef LOG_LEVEL
 #define LOG_LEVEL 0
 #endif
 
+static HWCDC& logSerial = Serial;
+
 #ifdef ENABLE_SERIAL_LOG
 #if LOG_LEVEL >= 0
-#define LOG_ERR(origin, format, ...) Serial.printf("[%lu] [ERR] [%s] " format "\n", millis(), origin, ##__VA_ARGS__)
+#define LOG_ERR(origin, format, ...) logSerial.printf("[%lu] [ERR] [%s] " format "\n", millis(), origin, ##__VA_ARGS__)
 #else
 #define LOG_ERR(origin, format, ...)
 #endif
 
 #if LOG_LEVEL >= 1
-#define LOG_INF(origin, format, ...) Serial.printf("[%lu] [INF] [%s] " format "\n", millis(), origin, ##__VA_ARGS__)
+#define LOG_INF(origin, format, ...) logSerial.printf("[%lu] [INF] [%s] " format "\n", millis(), origin, ##__VA_ARGS__)
 #else
 #define LOG_INF(origin, format, ...)
 #endif
 
 #if LOG_LEVEL >= 2
-#define LOG_DBG(origin, format, ...) Serial.printf("[%lu] [DBG] [%s] " format "\n", millis(), origin, ##__VA_ARGS__)
+#define LOG_DBG(origin, format, ...) logSerial.printf("[%lu] [DBG] [%s] " format "\n", millis(), origin, ##__VA_ARGS__)
 #else
 #define LOG_DBG(origin, format, ...)
 #endif
@@ -38,3 +50,20 @@
 #define LOG_ERR(origin, format, ...)
 #define LOG_INF(origin, format, ...)
 #endif
+
+class MySerialImpl : public Print {
+ public:
+  void begin(unsigned long baud) { logSerial.begin(baud); }
+
+  // Support boolean conversion for compatibility with code like:
+  //   if (Serial) or while (!Serial)
+  operator bool() const { return logSerial; }
+
+  __attribute__((deprecated("Use LOG_* macro instead"))) size_t printf(const char* format, ...);
+  size_t write(uint8_t b) override;
+  size_t write(const uint8_t* buffer, size_t size) override;
+  void flush() override;
+  static MySerialImpl instance;
+};
+
+#define Serial MySerialImpl::instance
