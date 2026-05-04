e09abadbfbc0b999ca2b6cb95f056b92e28938d3
jpirnay <jens@pirnay.com>
2026-05-04 16:52:54 +0200

    Mirror upstream PR#1817

--- lib/GfxRenderer/GfxRenderer.cpp
+++ lib/GfxRenderer/GfxRenderer.cpp
@@ -7,7 +7,10 @@
 #include <Utf8.h>
 #include <esp_heap_caps.h>
 
+#include <algorithm>
+
 #include "FontCacheManager.h"
+
 
 const uint8_t* GfxRenderer::getGlyphBitmap(const EpdFontData* fontData, const EpdGlyph* glyph) const {
   if (fontData->groups != nullptr) {
@@ -1696,16 +1699,8 @@ void GfxRenderer::fillPolygon(const int* xPoints, const int* yPoints, int numPoi
       j = i;
     }
 
-    // Sort nodes by X (simple bubble sort, numPoints is small)
-    for (int i = 0; i < nodes - 1; i++) {
-      for (int k = i + 1; k < nodes; k++) {
-        if (nodeX[i] > nodeX[k]) {
-          int temp = nodeX[i];
-          nodeX[i] = nodeX[k];
-          nodeX[k] = temp;
-        }
-      }
-    }
+    // Sort nodes by X
+    std::sort(nodeX, nodeX + nodes);
 
     // Fill between pairs of nodes
     for (int i = 0; i < nodes - 1; i += 2) {
